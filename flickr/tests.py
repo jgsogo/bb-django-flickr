97a401900760af9af276d3808fa19d53e150295b
Javier Garcia Sogo <jgsogo@gmail.com>
2012-05-26 01:14:08 +0200

    updating tests to make them suitable for new state

--- flickr/tests.py
+++ flickr/tests.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 # encoding: utf-8
 from datetime import datetime
-from django.contrib.auth.models import User
 from django.test import TestCase
 from django.test.client import Client
-from flickr.models import FlickrUser, Photo, PhotoSet
+from flickr.models import FlickrAccount, Photo, PhotoSet
 from flickr.tests_data import json_user, json_info, json_sizes, json_exif, \
     json_set_info, json_set_photos
 
@@ -13,19 +12,13 @@ class FlickrModelTests(TestCase):
 
     def setUp(self):
         self.client = Client()
-        self.user = User.objects.create(username='test', email='test@example.com')
-        self.user.set_password('test')
-        self.user.save()
-        self.flickr_user = FlickrUser.objects.create(user=self.user)
-        self.user2 = User.objects.create(username='test2', email='test2@example.com')
-        self.user2.set_password('test2')
-        self.user2.save()
-        self.flickr_user2 = FlickrUser.objects.create(user=self.user2)
+        self.flickr_user = FlickrAccount.objects.create()
+        self.flickr_user2 = FlickrAccount.objects.create()
 
 
     def test_user(self):
-        FlickrUser.objects.update_from_json(self.flickr_user.id, json_user)
-        fu = FlickrUser.objects.get(flickr_id=json_user['person']['id'])
+        FlickrAccount.objects.update_from_json(self.flickr_user.id, json_user)
+        fu = FlickrAccount.objects.get(flickr_id=json_user['person']['id'])
         self.assertEqual(fu.flickr_id, json_user['person']['id'])
         self.assertEqual(fu.nsid, json_user['person']['nsid'])
         self.assertEqual(fu.realname, json_user['person']['realname']['_content'])
@@ -36,7 +29,7 @@ class FlickrModelTests(TestCase):
 
 
     def test_photo_create(self):
-        photo = Photo.objects.create_from_json(flickr_user=self.flickr_user, info=json_info, sizes=json_sizes, exif=json_exif)
+        photo = Photo.objects.create_from_json(flickr_account=self.flickr_user, info=json_info, sizes=json_sizes, exif=json_exif)
         self.assertEqual(photo.flickr_id, json_info['photo']['id'])
         self.assertEqual(photo.title, json_info['photo']['title']['_content'])
         self.assertEqual(photo.server, json_info['photo']['server'])
@@ -56,7 +49,7 @@ class FlickrModelTests(TestCase):
 
 
     def test_photo_update(self):
-        photo = Photo.objects.create_from_json(flickr_user=self.flickr_user, info=json_info, sizes=json_sizes, exif=json_exif)
+        photo = Photo.objects.create_from_json(flickr_account=self.flickr_user, info=json_info, sizes=json_sizes, exif=json_exif)
         self.assertEqual(photo.title, json_info['photo']['title']['_content'])
         new_title = 'whoa that is a new title here'
         json_info['photo']['title']['_content'] = new_title
@@ -68,9 +61,9 @@ class FlickrModelTests(TestCase):
 
     def test_photoset(self):
 
-        photo = Photo.objects.create_from_json(flickr_user=self.flickr_user, info=json_info, sizes=json_sizes, exif=json_exif)
+        photo = Photo.objects.create_from_json(flickr_account=self.flickr_user, info=json_info, sizes=json_sizes, exif=json_exif)
 
-        photoset = PhotoSet.objects.create_from_json(flickr_user=self.flickr_user, info=json_set_info['photoset'], photos=json_set_photos)
+        photoset = PhotoSet.objects.create_from_json(flickr_account=self.flickr_user, info=json_set_info['photoset'], photos=json_set_photos)
         self.assertEqual(photoset.flickr_id, json_set_info['photoset']['id'])
         self.assertEqual(photoset.title, json_set_info['photoset']['title']['_content'])
         self.assertEqual(photoset.server, json_set_info['photoset']['server'])
